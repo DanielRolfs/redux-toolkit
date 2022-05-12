016c7a800fd4d8ff75f7fc68b6992faffdf5d85a
DanielRolfs <danielrolfs@icloud.com>
2022-05-12 09:26:45 +0200

    import cartitems into store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,9 @@ import {useSelector} from 'react-redux'
 
 
 const Navbar = () => {
-  console.log(
-    useSelector((store) => {
-      console.log(store);
-    })
-  );
-
+  
+  const amount = useSelector((store) => store.cart.amount)
+  
   return (
     <nav>
         <div className="nav-center">
@@ -16,7 +13,7 @@ const Navbar = () => {
             <div className="nav-container">
                 <CartIcon/>
                 <div className="amount-container">
-                    <p className="total-amount">0</p>
+                    <p className="total-amount">{amount}</p>
                 </div>
             </div>
         </div>
